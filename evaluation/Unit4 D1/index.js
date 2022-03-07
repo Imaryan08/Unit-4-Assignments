dda123afde9ccbb31450e600fb6ab50bc52e83c6
Rampukar Yadav <imaryan08@gmail.com>
2022-03-07 17:23:38 +0530

    name updated

--- evaluation/Unit4 D1/index.js
+++ evaluation/Unit4 D1/index.js
@@ -7,15 +7,30 @@ app.get('/books', (req,res) => {
     return res.send({ route: "/books"})
 });
 
-app.get('/libraries', (req,res) => {
-    return res.send({ route: "/libraries "})
+app.get('/libraries', permission('library'), (req,res) => {
+    return res.send({ route: "/libraries ", permission: req.role})
 });
 
-app.get('/authors', (req,res) => {
-    return res.send({ route: "/authors "})
+app.get('/authors', permission('writer'), (req,res) => {
+    return res.send({ route: "/authors ", permission: req.role})
 });
+
+function permission(check){
+    return function(req, res, next){
+        if(check === 'library' || check === 'writer')
+            req.role = true;
+        else
+            req.role = false;
+
+        next();
+    }
+}
+
 
 function logger(req,res,next) {
+    if( req.path === '/books' || req.path === '/authors' || req.path === '/libraries')
+        console.log('correct path');
+        
     next();
 }
 
